2eca4ab7c5468f6a181a4182dcaa1f94fc6801a5
NOOR <NOOR@DESKTOP-I3NE645>
2025-07-03 20:23:06 +0200

    Add project files.

---
+++ assignment03/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace assignment03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //Write a program that allows the user to enter a number then print it
+            //Console.Write("enter a number : ");
+            //int.TryParse(Console.ReadLine(), out int number);
+            //Console.WriteLine("you entered : " + number);
+            #endregion
+        }
+    }
+}
